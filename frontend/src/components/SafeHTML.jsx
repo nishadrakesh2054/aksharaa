6c8698277422f31a15e3906518eec83a281c016f
ArkshGroup <webdeveloper@arkshgroup.com>
2026-08-13 11:48:15 +0545

    dom fixed

--- frontend/src/components/SafeHTML.jsx
+++ frontend/src/components/SafeHTML.jsx
@@ -1,17 +1,5 @@
-// import React from "react";
-// import DOMPurify from "dompurify";
-
-// const SafeHTML = ({ htmlString }) => {
-//   const cleanHtmlString = DOMPurify.sanitize(htmlString);
-
-//   return <div dangerouslySetInnerHTML={{ __html: cleanHtmlString }} />;
-// };
-
-// export default SafeHTML;
-
 import React from "react";
-// import DOMPurify from "dompurify";
-import DOMPurify from "https://cdn.jsdelivr.net/npm/dompurify@3.0.3/dist/purify.es.min.js";
+import DOMPurify from "dompurify";
 
 
 const SafeHTML = ({ htmlString }) => {
